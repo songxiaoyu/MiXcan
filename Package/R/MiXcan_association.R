ad9d92f684c66a24afa3c9a9ca5c968d55661132
Xiaoyu Song <songxiaoyu152@gmail.com>
2023-05-31 16:22:11 -0700

    Update manual

--- Package/R/MiXcan_association.R
+++ Package/R/MiXcan_association.R
@@ -4,10 +4,10 @@
 #' level. Association analysis returns p-value at tissue level for inference purpose, and
 #' at cell-type level for post-inference (ad hoc) explaination of the likely source of
 #' association.
-#' @param MiXcan_predicted_expr: A N by 2 matrix indicating the predicted gene expression levels in two
+#' @param new_y: A N by 2 matrix indicating the predicted gene expression levels in two
 #' cell types. If a non-specific model is used for prediction, the predicted values should be the same in two cell types.
-#' @param covariates: A N by P matrix for the covariates in association analysis.
-#' @param outcome: The dependent variable of the association analysis. It can be continuous, categorical
+#' @param new_cov: A N by P matrix for the covariates in association analysis.
+#' @param new_outcome: The dependent variable of the association analysis. It can be continuous, categorical
 #' and count variables, and modeled through generalized linear models (GLM).
 #' @param family: Same as the family object used in GLM.
 #'
@@ -15,31 +15,27 @@
 #' and the combined p-value using the Cauchy combination.
 #' @export
 #'
-MiXcan_association <- function(MiXcan_predicted_expr, covariates, outcome, family){
-  MiXcan_association_dataframe <- cbind(MiXcan_prediction_result, covariates,  y = outcome)
-  MiXcan_association_dataframe_glm <- glm(as.formula(paste0("y ~.")), data = MiXcan_association_dataframe, family = family)
-  MiXcan_association_dataframe_glm_result <- broom::tidy(MiXcan_association_dataframe_glm)
-  MiXcan_association_dataframe_glm_result_cell_1 <- MiXcan_association_dataframe_glm_result %>%
-    filter(term == "cell_1")
-  MiXcan_association_dataframe_glm_result_cell_2 <- MiXcan_association_dataframe_glm_result %>%
-    filter(term == "cell_2")
-  if( cor(MiXcan_predicted_expr[,1],MiXcan_predicted_expr[,2]) > 0.99) {
-    MiXcan_association_dataframe_glm_result_cell_2 <- MiXcan_association_dataframe_glm_result_cell_1
+MiXcan_association <- function(new_y, new_cov,
+                               new_outcome, family= gaussian){
+  dat <- data.frame(new_y, new_cov,  y = new_outcome)
+  ft <- glm(as.formula(paste0("y ~.")), data = dat, family = family)
+  res <- broom::tidy(ft)
+  res1 <- res %>% filter(term == "cell_1")
+  res2 <- res %>% filter(term == "cell_2")
+  if( cor(new_y[,1],new_y[,2]) > 0.99) {res2 <- res1}
+  if( cor(new_y[,1],new_y[,2]) < -0.99) {
+    res2 <- res1 %>% mutate(estimate = -estimate)
   }
-  if( cor(MiXcan_predicted_expr[,1],MiXcan_predicted_expr[,2]) < -0.99) {
-    MiXcan_association_dataframe_glm_result_cell_2 <- MiXcan_association_dataframe_glm_result_cell_1 %>%
-      mutate(estimate = -estimate)
-  }
-  p_combined <- ACAT::ACAT(MiXcan_association_dataframe_glm_result_cell_1$p.value, MiXcan_association_dataframe_glm_result_cell_2$p.value)
-  MiXcan_association_dataframe_result <- MiXcan_association_dataframe_glm_result_cell_1 %>%
-    dplyr::select(cell_1_estimate = estimate,
-           cell_1_std.error = std.error,
-           cell_1_p = p.value) %>%
-    bind_cols(MiXcan_association_dataframe_glm_result_cell_2 %>%
-                dplyr::select(cell_2_estimate = estimate,
-                       cell_2_std.error = std.error,
-                       cell_2_p = p.value)) %>%
+  p_combined <- ACAT::ACAT(res1$p.value, res2$p.value)
+  result <- res1 %>%
+    dplyr::select(cell1_est = estimate,
+           cell1_se = std.error,
+           cell1_p = p.value) %>%
+    bind_cols(res2 %>%
+                dplyr::select(cell2_est = estimate,
+                       cell2_se = std.error,
+                       cell2_p = p.value)) %>%
     mutate(p_combined = p_combined) %>%
     as.data.frame()
-  return(MiXcan_association_dataframe_result)
+  return(result)
 }
